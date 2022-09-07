b2b33c4b558e7688ed45a3759e8ce66f036d5634
Veda Vyas <vedavyas.y@fortunaidentity.com>
2022-09-07 19:52:20 +0530

    added code for values, types and operators

---
+++ values_types_operators/personal_details.js
@@ -0,0 +1,19 @@
+var name = "Veda Vyas";
+var email = "vedavyas.yeleswarapu@gmail.com";
+var bDate = 24;
+var bMonth = 04;
+var bYear = 1972;
+var gender = "Male";
+var maritalStatus = "Single";
+var phoneNumber = 9999999999;
+var jobDescription = "I am a UI Designer";
+var address = "Dubai";
+
+console.log(jobDescription);
+console.log(name);
+console.log(email);
+console.log(bDate+"/"+bMonth+"/"+bYear);
+console.log(gender);
+console.log(maritalStatus);
+console.log(phoneNumber);
+console.log(address);
