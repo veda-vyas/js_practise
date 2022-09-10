8125b5edac51e1d56ada1344ba36c1aebcdc9b36
Veda Vyas <vedavyas.y@fortunaidentity.com>
2022-09-10 16:00:56 +0530

    statements and expressions

---
+++ program_structure/statements_and_expressions.js
@@ -0,0 +1,11 @@
+// Arithmetic Operator (Sum)
+var maths_marks = 68;
+var english_marks = 94;
+var cs_marks = 88;
+var javascript_marks = 95;
+
+var total_marks = ( maths_marks + 
+                english_marks + 
+                cs_marks + 
+                javascript_marks );
+console.log(total_marks);
